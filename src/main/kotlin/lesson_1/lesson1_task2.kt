894b9413e2ac0fcdbedc142c2824c1cadb0b8f30
Gennady <avenkar@gmail.com>
2025-01-22 20:48:56 +0300

    add variable for shop staff and print it

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,12 @@
+package org.example.lesson_1
+
+fun main() {
+    var orderSummary: Int = 20
+    val checkoutMessage: String = "Спасибо за покупку"
+    println(orderSummary)
+    println(checkoutMessage)
+    var shopStaff: Int = 2000
+//    println(shopStaff)
+    shopStaff = 1999
+    println(shopStaff)
+}
