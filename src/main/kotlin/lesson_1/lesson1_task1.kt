a157ebdf5ccf495fbf89d243c69eaef5a1ac7a00
Gennady <avenkar@gmail.com>
2025-01-23 16:20:59 +0300

    KS-1-1-add-variables-and-rename-task-file

--- src/main/kotlin/lesson_1/lesson1_task1.kt
+++
@@ -1,8 +0,0 @@
-package org.example.lesson_1
-
-fun main() {
-    var orderSummary: Int = 20
-    val checkoutMessage: String = "Спасибо за покупку"
-    println(orderSummary)
-    println(checkoutMessage)
-}
