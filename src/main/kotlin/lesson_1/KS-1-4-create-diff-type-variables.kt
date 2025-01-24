31b188526a67dbe318f27711628db8d002d62dda
Gennady <avenkar@gmail.com>
2025-01-24 15:26:46 +0300

    KS-1-4-add-variables-with-different-types

---
+++ src/main/kotlin/lesson_1/KS-1-4-create-diff-type-variables.kt
@@ -0,0 +1,17 @@
+package org.example.lesson_1
+
+fun main() {
+    val length = 40_868_600_000L
+    var age: Byte = 27
+    val partOfDay = 0.075f
+    var seconds: Short = 6480
+    val partOfYear = 2.0547945205479453E-4
+    var apogee: Int = 327000
+
+    println(length)
+    println(age)
+    println(partOfDay)
+    println(seconds)
+    println(partOfYear)
+    println(apogee)
+}
