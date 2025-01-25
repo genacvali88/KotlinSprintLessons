7a861353c7edb198587589eca5acd4698b29a325
gennady <avenkar@gmail.com>
2025-01-25 14:24:26 +0300

    KS-1-5-rework-the-code-add-constants

--- src/main/kotlin/lesson_1/KS-1-5-calculate-time-from-seconds.kt
+++ src/main/kotlin/lesson_1/KS-1-5-calculate-time-from-seconds.kt
@@ -1,11 +1,16 @@
 package org.example.lesson_1
 
+const val MINUTES_IN_HOUR = 60
+const val SECONDS_IN_MINUTE = 60
+
 fun main() {
     val totalSeconds = 6480
-    val hours = totalSeconds / 3600
-    val remainingSecondsAfterHours = totalSeconds % 3600
-    val minutes = remainingSecondsAfterHours / 60
-    val seconds = remainingSecondsAfterHours % 60
+
+    val hours = totalSeconds / (MINUTES_IN_HOUR * SECONDS_IN_MINUTE)
+    val remainingSecondsAfterHours = totalSeconds % (MINUTES_IN_HOUR * SECONDS_IN_MINUTE)
+
+    val minutes = remainingSecondsAfterHours / SECONDS_IN_MINUTE
+    val seconds = remainingSecondsAfterHours % SECONDS_IN_MINUTE
 
     val formattedTime = String.format("%02d:%02d:%02d", hours, minutes, seconds)
     println(formattedTime)
